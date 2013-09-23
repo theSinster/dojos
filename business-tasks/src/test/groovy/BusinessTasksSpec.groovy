781136f029852e97c55abb73e9086d85f4be50fd
Sönke Nommensen <snommensen@gmail.com>
2013-09-23 22:43:26 +0200

    Added another test case.

--- business-tasks/src/test/groovy/BusinessTasksSpec.groovy
+++ business-tasks/src/test/groovy/BusinessTasksSpec.groovy
@@ -9,21 +9,35 @@ class BusinessTasksSpec extends Specification {
         businessTasks = new BusinessTasks()
     }
 
+    def "the index is calculated correctly"() {
+        expect:
+        businessTasks.calculateListIndex(listSize, n) == result
+
+        where:
+        listSize | n    | result
+        4        | 2    | 1
+        2        | 1000 | 1
+    }
+
+    @Unroll
     def "return the tasks which the business man chooses to execute"() {
         expect:
         businessTasks.getTask(list as String[], n) == result
 
         where:
-        list                                               | n       | result
-        ["a", "b", "c", "d"]                               | 2       | "a"
-        ["a", "b", "c", "d", "e"]                          | 3       | "d"
-        ["alpha", "beta", "gamma", "delta", "epsilon"]     | 1       | "epsilon"
-        ["a", "b"]                                         | 1000    | "a"
+        list                                                          | n       | result
+        ["a", "b", "c", "d"]                                          | 2       | "a"
+        ["a", "b", "c", "d", "e"]                                     | 3       | "d"
+        ["alpha", "beta", "gamma", "delta", "epsilon"]                | 1       | "epsilon"
+        ["a", "b"]                                                    | 1000    | "a"
         ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-                "u", "v", "w", "x", "y", "z"]              | 17      | "n"
+                "u", "v", "w", "x", "y", "z"]                         | 17      | "n"
         ["zlqamum", "yjsrpybmq", "tjllfea", "fxjqzznvg", "nvhekxr", "am", "skmazcey", "piklp",
                 "olcqvhg", "dnpo", "bhcfc", "y", "h", "fj", "bjeoaxglt", "oafduixsz", "kmtbaxu",
-                "qgcxjbfx", "my", "mlhy", "bt", "bo", "q"] | 9000000 | "fxjqzznvg"
+                "qgcxjbfx", "my", "mlhy", "bt", "bo", "q"]            | 9000000 | "fxjqzznvg"
+        ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w",
+                "v", "x", "y", "aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo",
+                "pp", "qq", "rr", "ss", "tt", "uu", "ww", "vv", "xx"] | 9999993 | "gg"
 
     }
 }
