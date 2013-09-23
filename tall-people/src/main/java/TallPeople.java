a4fc05a334b2927c4c13d155a87f20246d733c9a
Sönke Nommensen <snommensen@gmail.com>
2013-09-23 22:44:21 +0200

    Added spec for tall people problem.

---
+++ tall-people/src/main/java/TallPeople.java
@@ -0,0 +1,8 @@
+public class TallPeople {
+
+    public int[] getPeople(String[] people) {
+        // TODO http://community.topcoder.com/stat?c=problem_statement&pm=2923&rd=5854
+        return new int[2];
+    }
+}
+
---
+++ tall-people/src/test/groovy/TallPeopleSpec.groovy
@@ -0,0 +1,23 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class TallPeopleSpec extends Specification {
+
+    TallPeople tallPeople
+
+    def setup() {
+        tallPeople = new TallPeople()
+    }
+
+    @Unroll
+    def "return the tasks which the business man chooses to execute"() {
+        expect:
+        tallPeople.getPeople(poeple as String[]) == result
+
+        where:
+        poeple                | result
+        ["9 2 3", "4 8 7"]    | [4, 7]
+        ["1 2", "4 5", "3 6"] | [4, 4]
+        ["1 1", "1 1"]        | [1, 1]
+    }
+}
