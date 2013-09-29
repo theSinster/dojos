d177bb7ba643ab09f8779bb4ca2d2a92aa5ab8c1
sina <sina.scheuplein@gmail.com>
2013-09-29 21:45:25 +0200

    First shot: iterative solution

--- business-tasks/src/main/java/BusinessTasks.java
+++ business-tasks/src/main/java/BusinessTasks.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BusinessTasks {
 
     public String getTask(String[] list, int n) {
-        // TODO http://community.topcoder.com/stat?c=problem_statement&pm=1585&rd=6535
-        return null;
+        ArrayList<String> taskList = new ArrayList<>(Arrays.asList(list));
+
+        int indexToBeRemoved = 0;
+        while (taskList.size() > 1) {
+            indexToBeRemoved = calculateIndexToBeRemoved(taskList.size(), n, indexToBeRemoved);
+            taskList.remove(indexToBeRemoved);
+        }
+        return taskList.get(0);
+    }
+
+    private int calculateIndexToBeRemoved(int sizeOfList, int seed, int currentIndex) {
+        int newPosition = (seed + currentIndex) % sizeOfList;
+        if (newPosition == 0) {
+            return sizeOfList - 1;
+        }
+        return newPosition - 1;
     }
 }
